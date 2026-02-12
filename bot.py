c7fcfacdaa0d264e5ffbc528a4b1ae861b285894
Oleksandr <slevkiskiy921@gmail.com>
2026-02-12 08:51:06 +0200

    Add Weather

--- bot.py
+++ bot.py
@@ -1,34 +1,69 @@
 import asyncio
 import logging
 import os
+import requests  # Щоб ходити в інтернет
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 # 1. Завантажуємо секрети
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
+WEATHER_KEY = os.getenv("WEATHER_API_KEY")
 
-# 2. Перевірка на дурня (чи є токен)
+# 2. Перевірка
 if not TOKEN:
-    exit("Error: BOT_TOKEN not found! Check your .env file.")
+    exit("Error: BOT_TOKEN not found!")
 
 # 3. Налаштування
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# 4. Реакція на /start
+# --- КЛАВІАТУРА (КНОПКИ) ---
+kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🌦 Погода Брусилів")],
+        [KeyboardButton(text="💵 Курс Долара (Скоро)")]
+    ],
+    resize_keyboard=True
+)
+
+# --- ФУНКЦІЯ ПОГОДИ ---
+def get_weather(city="Brusyliv"):
+    try:
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_KEY}&units=metric&lang=ua"
+        res = requests.get(url).json()
+        
+        temp = res["main"]["temp"]
+        desc = res["weather"][0]["description"]
+        wind = res["wind"]["speed"]
+        
+        return f"Погода в {city}:\n🌡 Температура: {temp}°C\n☁️ Небо: {desc}\n💨 Вітер: {wind} м/с"
+    except Exception as e:
+        return "⚠️ Не можу отримати погоду. Перевір API ключ."
+
+# --- ОБРОБНИКИ (HANDLERS) ---
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Привіт! Я живий! 🚀\nЯ працюю в Docker на AWS.\nЯ оновився САМ через GitHub Actions! 😎")
+    await message.answer(
+        "Привіт, Саня! Я тепер розумний бот. 🤖\nТисни кнопки знизу!", 
+        reply_markup=kb
+    )
 
-# 5. Ехо (повторює текст)
+@dp.message(F.text == "🌦 Погода Брусилів")
+async def weather_handler(message: types.Message):
+    await message.answer("Секунду, дивлюсь у вікно... 🔭")
+    report = get_weather("Brusyliv")
+    await message.answer(report)
+
 @dp.message()
 async def echo_handler(message: types.Message):
-    await message.answer(f"Ти написав: {message.text}")
+    await message.answer("Я поки розумію тільки кнопки! 👇", reply_markup=kb)
 
-# 6. Запуск
+# --- ЗАПУСК ---
 async def main():
     await dp.start_polling(bot)
 
